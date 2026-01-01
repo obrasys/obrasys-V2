a998b6116b199e916fc6e1d75a02163f87c09700
[dyad] <snapimoveis@gmail.com>
2026-01-01 17:47:42 -0300

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./globals.css";
 import { setDefaultOptions } from "date-fns";
 import { pt } from "date-fns/locale";
+import { SessionContextProvider } from "./components/SessionContextProvider";
 
 // Set default locale for date-fns
 setDefaultOptions({ locale: pt });
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <SessionContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </SessionContextProvider>
+);
